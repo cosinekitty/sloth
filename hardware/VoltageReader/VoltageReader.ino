17bbef58a9370561556a93664c091e67cdaaedb3
Don Cross <cosinekitty@gmail.com>
2023-09-13 20:54:50 -0400

    Arduino software to log Torpor Sloth voltages.

--- hardware/VoltageReader/VoltageReader.ino
+++ hardware/VoltageReader/VoltageReader.ino
@@ -6,10 +6,31 @@
     with the real thing.
 */
 
+const unsigned long IntervalMillis = 200;
+unsigned long TargetMillis;
+
 void setup()
 {
+    Serial.begin(115200);
+    TargetMillis = millis() + IntervalMillis;
 }
 
 void loop()
 {
+    unsigned long now;
+    do { now = millis(); } while (now != TargetMillis);     // risky but should be OK
+    TargetMillis = now + IntervalMillis;
+
+    int vx = analogRead(A0);
+    int vy = analogRead(A1);
+    int vz = analogRead(A2);
+
+    Serial.print(now);
+    Serial.print(",");
+    Serial.print(vx);
+    Serial.print(",");
+    Serial.print(vy);
+    Serial.print(",");
+    Serial.print(vz);
+    Serial.println();
 }
