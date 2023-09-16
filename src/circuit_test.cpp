1117a9ee3d704d6dad493e205d014661f206ce38
Don Cross <cosinekitty@gmail.com>
2023-09-15 20:06:31 -0400

    Measure real time required to simulate the Sloth circuit.

--- src/circuit_test.cpp
+++ src/circuit_test.cpp
@@ -5,6 +5,38 @@
 #include "circuit.hpp"
 #include "torpor_sloth_circuit.hpp"
 
+#if defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+#include <sys/time.h>
+#elif defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
+#include <time.h>
+#endif
+
+double TimeInSeconds()
+{
+    double sec;         // Seconds since midnight January 1, 1970.
+
+#if defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    sec = (double)tv.tv_sec + tv.tv_usec/1.0e+6;
+#elif defined(_WIN32)
+    FILETIME ft;
+    ULARGE_INTEGER large;
+    // Get time in 100-nanosecond units from January 1, 1601.
+    GetSystemTimePreciseAsFileTime(&ft);
+    large.u.LowPart  = ft.dwLowDateTime;
+    large.u.HighPart = ft.dwHighDateTime;
+    sec = (large.QuadPart - 116444736000000000ULL) / 1.0e+7;
+#else
+    #error Microsecond time resolution is not supported on this platform.
+#endif
+
+    return sec;
+}
+
 const int SAMPLE_RATE = 44100;
 
 template <typename real_t>
@@ -280,6 +312,7 @@ static int UnitTest_Torpor()
     circuit.setControlVoltage(-1.3);
     circuit.setKnobPosition(0.25);
 
+    double startTime = TimeInSeconds();
     const int nseconds = 10;
     const int nsamples = nseconds * SAMPLE_RATE;
     for (int sample = 0; sample < nsamples; ++sample)
@@ -314,9 +347,10 @@ static int UnitTest_Torpor()
             return 1;
         }
     }
+    double elapsedTime = TimeInSeconds() - startTime;
 
     PerformanceStats stats = circuit.getPerformanceStats();
 
-    printf("Torpor: PASS -- mean_iter=%lg\n", stats.meanIterationsPerSample());
+    printf("Torpor: PASS -- mean_iter=%lg, simulated %d seconds in %0.3lf seconds of real time.\n", stats.meanIterationsPerSample(), nseconds, elapsedTime);
     return 0;
 }
