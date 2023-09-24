eaf80f840876749496f6ca537b7f1a1d09334d54
Don Cross <cosinekitty@gmail.com>
2023-09-23 23:07:27 -0400

    Generate blank panel for Sloth Torpor.

--- panel/makepanel.py
+++ panel/makepanel.py
@@ -7,10 +7,25 @@
 import sys
 from svgpanel import *
 
+def Print(message:str) -> int:
+    print('makepanel.py:', message)
+    return 0
+
+def Save(panel:Panel, filename:str) -> int:
+    panel.save(filename)
+    return Print('Wrote: ' + filename)
+
 PANEL_WIDTH = 4
+PANEL_COLOR  = '#e6e5e5'
+BORDER_COLOR = '#e6e5e5'
 
 def GenerateMainPanel() -> int:
-    return 0
+    svgFileName = '/home/don/github/nonlinearcircuits/res/SlothTorpor.svg'
+    panel = Panel(PANEL_WIDTH)
+    pl = Element('g', 'PanelLayer')
+    pl.append(BorderRect(PANEL_WIDTH, PANEL_COLOR, BORDER_COLOR))
+    panel.append(pl)
+    return Save(panel, svgFileName)
 
 if __name__ == '__main__':
     sys.exit(GenerateMainPanel())
