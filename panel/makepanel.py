9d7f17879471dca433dc7ce47f75efe669b5ce9b
Don Cross <cosinekitty@gmail.com>
2023-09-23 22:56:02 -0400

    Starting to make an SVG panel design generator for Sloth Torpor.

---
+++ panel/makepanel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+#   makepanel.py  -  Don Cross <cosinekitty@gmail.com>
+#
+#   Generate the SVG panel design for the VCV Rack version of Sloth Torpor
+#
+import sys
+from svgpanel import *
+
+PANEL_WIDTH = 4
+
+def GenerateMainPanel() -> int:
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(GenerateMainPanel())
